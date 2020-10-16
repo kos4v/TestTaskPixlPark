dc7cbd0327d5fc6fbc8e1125a105f26f7385ad55
Иван Косачев <kosach_1@live.com>
2020-10-16 20:37:06 +0700

    получены заказы

--- TestTaskPixlPark/Controllers/HomeController.cs
+++ TestTaskPixlPark/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -22,30 +25,77 @@ namespace TestTaskPixlPark.Controllers
             _logger = logger;
         }
 
-        static async Task<Product> GetProductAsync(string path)
-        {
-            Product product = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                return await response.Content.ReadAsAsync<Product>();
-            }
-            return product;
-        }
-
+        [HttpGet]
         public async Task<IActionResult> IndexAsync()
         {
-            var output = await GetProductAsync("http://api.pixlpark.com/oauth/requesttoken");
-            System.Diagnostics.Debug.WriteLine("");
-            System.Diagnostics.Debug.WriteLine("");
-            System.Diagnostics.Debug.WriteLine("");
+            string publicKey = "38cd79b5f2b2486d86f562e3c43034f8";
+            string privateKey = "8e49ff607b1f46e1a5e8f6ad5d312a80";
+            string output = await GetAccessKey(publicKey, privateKey);
             System.Diagnostics.Debug.WriteLine(output);
-            System.Diagnostics.Debug.WriteLine("xxx");
-            System.Diagnostics.Debug.WriteLine("");
-            System.Diagnostics.Debug.WriteLine("");
-            System.Diagnostics.Debug.WriteLine("");
-
+            System.Diagnostics.Debug.WriteLine("end");
+            GetProduct(output);
             return View();
+        }
+
+        static async Task<string> GetProduct(string accessKey)
+        {
+            string path = "http://api.pixlpark.com/orders?oauth_token="+ accessKey;
+            HttpResponseMessage response = await client.GetAsync(path);
+            ResponseOrders responseOrders;
+            if (response.IsSuccessStatusCode)
+            {
+                var res = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine(res);
+                System.Diagnostics.Debug.WriteLine("endorders");
+                //var responseOrders = JsonSerializer.Deserialize<ResponseOrders>(res);
+                responseOrders = await response.Content.ReadAsAsync<ResponseOrders>();
+                System.Diagnostics.Debug.WriteLine(responseOrders.ApiVersion);
+                System.Diagnostics.Debug.WriteLine(responseOrders.Result[0].CustomId);
+
+            }
+            return "Hello";
+        }
+
+
+
+        static async Task<string> GetAccessKey(string publicKey, string privateKey)
+        {
+            string path = "http://api.pixlpark.com/oauth/requesttoken";
+            HttpResponseMessage response = await client.GetAsync(path);
+            ResponseAuth responseRequest =  new ResponseAuth();
+            if (response.IsSuccessStatusCode)
+            {
+                responseRequest = await response.Content.ReadAsAsync<ResponseAuth>();
+            }
+
+            
+            StringBuilder password;
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(responseRequest.RequestToken + privateKey));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte s in hash) sb.Append(s.ToString("x2"));
+                System.Diagnostics.Debug.WriteLine(sb);
+                password = sb;
+            }
+            string AccessUrl = "http://api.pixlpark.com/oauth/accesstoken/?oauth_token=" + responseRequest.RequestToken +
+                "&username=" + publicKey +
+                "&password=" + password;
+
+            System.Diagnostics.Debug.WriteLine(AccessUrl);
+
+
+            ResponseAuth responseAccess;
+            HttpResponseMessage responseAcccess = await client.GetAsync(AccessUrl);
+            if (responseAcccess.IsSuccessStatusCode)
+            {
+                var res1 = await responseAcccess.Content.ReadAsStringAsync();
+                responseAccess = await responseAcccess.Content.ReadAsAsync<ResponseAuth>();
+                System.Diagnostics.Debug.WriteLine(res1);
+                return responseAccess.AccessToken;
+            }
+            return "Unsuccess";
         }
 
 
@@ -59,15 +109,6 @@ namespace TestTaskPixlPark.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
-
-
-    public class Product
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Category { get; set; }
     }
 
 
---
+++ TestTaskPixlPark/Models/Responses.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTaskPixlPark.Models
+{
+    public class ResponseAuth
+    {
+        public string RequestToken { get; set; }
+        public string AccessToken { get; set; }
+        public string Expires { get; set; }
+        public string RefreshToken { get; set; }
+        public string Scope { get; set; }
+        public string Error { get; set; }
+        public string Success { get; set; }
+        public string RequireSsl { get; set; }
+
+    }
+
+    public class ResponseOrders
+    {
+        public string ApiVersion { get; set; }
+        public string ResponseCode { get; set; }
+        public Product[] Result { get; set; }
+
+    }
+
+    public class DeliveryAddress
+    {
+        public string ZipCode { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+
+    }
+
+    public class Shipping
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ShippingType { get; set; }
+    }
+
+    public class Product
+    {
+        public string Id { get; set; }
+        public string CustomId { get; set; }
+        public string SourceOrderId { get; set; }
+        public string Title { get; set; }
+        public string TrackingUrl { get; set; }
+        public string Status { get; set; }
+        public string RenderStatus { get; set; }
+        public string PaymentStatus { get; set; }
+        public string CommentsCount { get; set; }
+        public string DownloadLink { get; set; }
+        public string PreviewImageUrl { get; set; }
+        public string Price { get; set; }
+        public string DiscountPrice { get; set; }
+        public string DeliveryPrice { get; set; }
+        public string TotalPrice { get; set; }
+        public string UserId { get; set; }
+        public string UserCompanyAccountId { get; set; }
+        public string DiscountId { get; set; }
+        public string DiscountTitle { get; set; }
+        public string DateCreated { get; set; }
+        public string DateModified { get; set; }
+        public string DatePaid { get; set; }
+        public DeliveryAddress DeliveryAddress { get; set; }
+        public Shipping Shipping { get; set; }
+
+    }
+
+}
